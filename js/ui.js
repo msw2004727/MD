2efc21fc17dc951587fcdcb10770d5cd396710a5
msw2004727 <msw741121@gmail.com>
2025-06-11 22:07:36 +0800

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -1015,7 +1015,7 @@ function renderMonsterFarm() {
         item.querySelector('.farm-monster-name-link').addEventListener('click', (e) => {
             e.preventDefault();
             e.stopPropagation();
-            updateMonsterInfoModal(monster, gameState.gameConfigs, gameState.playerData);
+            updateMonsterInfoModal(monster, gameState.gameConfigs);
             showModal('monster-info-modal');
         });
 
